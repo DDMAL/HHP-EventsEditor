f349918d178873361398c902195ff759149cab92
Jian-23 <Jian-23@users.noreply.github.com>
2015-08-22 16:31:18 -0400

    Update filter.java

--- Issue/filter.java
+++ Issue/filter.java
@@ -12,7 +12,7 @@ public class T {
 	final static Charset ENCODING = StandardCharsets.UTF_8;
 	
 	public static void main(String[] args) throws IOException {
-	//Environment setting of StanfordNLP
+	    //Environment setting of StanfordNLP
 	    Properties props = new Properties();
 	    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
 	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
